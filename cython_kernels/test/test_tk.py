b13d9dd8b84d62f7575a602524c19fc7a4130967
beckdaniel <daniel.e.beck@gmail.com>
2014-01-20 18:37:01 +0000

    more implementation and prof tests

--- cython_kernels/test/test_tk.py
+++ cython_kernels/test/test_tk.py
@@ -3,10 +3,11 @@
 import unittest
 import nltk
 from cython_kernels.tk.sst import SubsetTreeKernel as SST
+import datetime
 
 class SSTTests(unittest.TestCase):
 
-    def test_parse(self):
+    def test_gen_node_list(self):
         repr1 = "(S (NP (ADJ colorless) (N ideas)) (VP (V sleep) (ADV furiously)))"
         k = SST()
         nodes1 = k._gen_node_list(repr1)
@@ -14,6 +15,31 @@ class SSTTests(unittest.TestCase):
         self.assertEqual(str(nodes1), result)
         #print nodes1
 
+    def test_get_node_pair_list(self):
+        repr1 = "(S (NP (ADJ colorless) (N ideas)) (VP (V sleep) (ADV furiously)))"
+        repr2 = "(S (NP (ADJ colorless) (N ideas)) (VP (V sleep) (ADV furiously)))"
+        k = SST()
+        nodes1 = k._gen_node_list(repr1)
+        nodes2 = k._gen_node_list(repr2)
+        node_list = k._get_node_pair_list(nodes1, nodes2)
+        result = "[((0, 0), (0, 0), 0), ((1, 1), (1, 1), 0), ((0, 1), (0, 1), 0), ((1, 0), (1, 0), 0), ((0,), (0,), 2), ((1,), (1,), 2), ((), (), 2)]"
+        self.assertEqual(str(node_list), result)
+        #print node_list
+
+
+class SSTProfilingTests(unittest.TestCase):
+
+    def test_prof_gen_node_pair_list(self):
+        repr1 = "(S (NP (ADJ colorless) (N ideas)) (VP (V sleep) (ADV furiously)))"
+        repr2 = "(S (NP (ADJ colorless) (N ideas)) (VP (V sleep) (ADV furiously)))"
+        k = SST()
+        nodes1 = k._gen_node_list(repr1)
+        nodes2 = k._gen_node_list(repr2)
+        start_time = datetime.datetime.now()
+        for i in range(100000):
+            node_list = k._get_node_pair_list(nodes1, nodes2)
+        end_time = datetime.datetime.now()
+        print end_time - start_time
 
 if __name__ == "__main__":
     unittest.main()
--- cython_kernels/tk/sst.py
+++ cython_kernels/tk/sst.py
@@ -10,14 +10,42 @@ class SubsetTreeKernel(object):
     def __init__(self, _lambda=0.1, _sigma=1):
         self._lambda = _lambda
         self._sigma = _sigma
+        self._tree_cache = {}
 
     def _gen_node_list(self, tree_repr):
         tree = nltk.Tree(tree_repr)
         pos = tree.treepositions()
         for l in tree.treepositions(order="leaves"):
             pos.remove(l)
-        z = zip(tree.productions(), pos)
-        z.sort()
-        return z
-        
+        node_list = zip(tree.productions(), pos)
+        node_list.sort()
+        return node_list
+
+    def _get_node_pair_list(self, nodes1, nodes2):
+        node_pair_list = []
+        i1 = 0
+        i2 = 0
+        while True:
+            try:
+                if nodes1[i1][0] > nodes2[i2][0]:
+                    i2 += 1
+                elif nodes1[i1][0] < nodes2[i2][0]:
+                    i1 += 1
+                else:
+                    while nodes1[i1][0] == nodes2[i2][0]:
+                        reset2 = i2
+                        while nodes1[i1][0] == nodes2[i2][0]:
+                            if type(nodes1[i1][0].rhs()[0]) == str:
+                                # We consider preterms as leaves
+                                tup = (nodes1[i1][1], nodes2[i2][1], 0)
+                            else:
+                                tup = (nodes1[i1][1], nodes2[i2][1], len(nodes1[i1][0].rhs()))
+                            node_pair_list.append(tup)
+                            i2 += 1
+                        i1 += 1
+                        i2 = reset2
+            except IndexError:
+                break
+        node_pair_list.sort(key=lambda x: len(x[0]), reverse=True)
+        return node_pair_list
         
